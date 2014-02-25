721226826a9e34e6ced87fd70ed1a9523e329c97
Doctor Cobweb <spinninghalf@gmail.com>
2014-02-25 21:02:36 +1100

    pimped out adminConsole. can now add and remove filters

--- server.js
+++ server.js
@@ -88,20 +88,6 @@ app.configure(function () {
 app.post('/sessions', function (req, res) {
     console.log('in POST /sessions and body posted is:');
     console.log(req.body);
-    // used to seed the capersdb initially
-    /*
-    var adminUser = new AdminUserModel({
-        username: 'andre',
-        password: 'andre'
-    });
-    adminUser.save(function (err) {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('SUCCESS: saved andre as AdminUser');
-        }
-    });
-    */
 
     var query = {username: req.body.username};
 
@@ -146,9 +132,131 @@ app.post('/sessions', function (req, res) {
 });
 
 
+app.get('/adminConsole/filterDelete/:id', function (req, res) {
+    console.log('in GET /adminConsole/filterDelete/:id route handler');
+    console.log('req.params.id: ' + req.params.id);
+
+    // delete the filter using the passed in id parameter which is the document's _id
+    FilterModel.findById(req.params.id, function (err, filter) {
+        if (err) throw err;
+        return filter.remove(function (err) {
+            if (err) throw err;
+            console.log('DELETED filter: ' + filter);
+            return res.redirect('/adminConsole');
+        });
+    });
+})
+
 
 
 app.get('/adminConsole', loggedInAsAdmin, function (req, res) {
+    console.log('in GET /adminConsole route handler');
+
+    // find all the filter terms
+    FilterModel.find(function (err, filters) {
+        if (!err) {
+            //console.log('FOUND FILTERS:');
+            //console.log(filters);
+            render(filters);         
+        } else {
+            console.log('ERROR: COULD NOT FIND FILTERS:');
+            console.log(err);
+        }
+    });
+
+
+    function render (filters) {
+        jade.renderFile('./views/adminConsole.jade', {filters: filters}, cb);
+
+        function cb (err, html) {
+            if (err) throw err;
+            return res.send(html);
+        }
+    }
+});
+
+
+
+app.get('/filters', function (req, res) {
+    console.log('in GET /filters route handler');
+
+    return FilterModel.find(function (err, filters) {
+        if (!err) {
+            return res.send(filters);         
+        } else {
+            console.log(err);
+            return res.send(err);
+        }
+    });
+
+});
+
+
+
+
+app.get('/logout', loggedInAsAdmin, function (req, res) {
+    console.log('in GET /logout route handler');
+    req.session = void 0;    
+    console.log('LOGGED USER OUT. req.session set to undefined. Check to see:');
+    console.log(req.session);
+
+    return res.redirect('/');
+});
+
+
+app.post('/addFilter', function (req, res) {
+    console.log('in POST /addFilter route handler');
+
+    var filter = new FilterModel({
+        badTerm: req.body.badTerm,
+        goodTerm: req.body.goodTerm,
+        description: req.body.description
+    });
+  
+    filter.save(function (err) {
+        if (err) throw err;
+
+        console.log('SUCCESS: added a new filter using the admin console.');
+        console.log('new filter contents are:');
+        console.log(req.body);
+
+        //return res.send(req.body);
+        return res.redirect('adminConsole');
+    });
+});
+
+
+
+
+
+// start the server
+app.listen(PORT, function () {
+    console.log('HTTP express server listening on port %d in %s mode',
+        PORT, app.settings.env);
+    console.log('Serving client app from: ' + applicationRoot + '/' + CLIENT_APP_DIR);
+
+});
+
+
+
+
+
+// helper functions
+
+function loggedInAsAdmin (req, res, next) {
+    if (!!req.session.authenticated) {
+        next();
+    } else {
+        return res.render('unauthorizedAccess');
+    }
+}
+
+
+
+
+
+// old junk
+
     // used to seed the capersdb initially
     /*
     var filter = new FilterModel({
@@ -191,59 +299,20 @@ app.get('/adminConsole', loggedInAsAdmin, function (req, res) {
     });
     */
 
-    // find all the filter terms
-    //filters is an array
-    FilterModel.find(function (err, filters) {
-        if (!err) {
-            console.log('FOUND FILTERS:');
-            console.log(filters);
-            render(filters);         
-        } else {
-            console.log('ERROR: COULD NOT FID FILTERS:');
+
+
+    // used to seed the capersdb initially
+    /*
+    var adminUser = new AdminUserModel({
+        username: 'andre',
+        password: 'andre'
+    });
+    adminUser.save(function (err) {
+        if (err) {
             console.log(err);
+        } else {
+            console.log('SUCCESS: saved andre as AdminUser');
         }
     });
+    */
 
-
-    function render (filters) {
-        jade.renderFile('./views/adminConsole.jade', {filters: filters}, cb);
-
-        function cb (err, html) {
-            if (err) throw err;
-            return res.send(html);
-        }
-    }
-});
-
-
-app.get('/logout', loggedInAsAdmin, function (req, res) {
-    req.session = void 0;    
-    console.log('LOGGED USER OUT. req.session set to undefined. Check to see:');
-    console.log(req.session);
-
-    return res.redirect('/');
-});
-
-
-
-// start the server
-app.listen(PORT, function () {
-    console.log('HTTP express server listening on port %d in %s mode',
-        PORT, app.settings.env);
-    console.log('Serving client app from: ' + applicationRoot + '/' + CLIENT_APP_DIR);
-
-});
-
-
-
-
-
-// helper functions
-
-function loggedInAsAdmin (req, res, next) {
-    if (!!req.session.authenticated) {
-        next();
-    } else {
-        return res.render('unauthorizedAccess');
-    }
-}
